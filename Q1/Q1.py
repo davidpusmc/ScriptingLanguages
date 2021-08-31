76e4e5706becf33d44a6fcd015686629b0099106
david <davidpusmc@gmail.com>
2021-08-31 13:06:38 -0500

    changes to this branch

---
+++ Q1/Q1.py
@@ -0,0 +1,7 @@
+int(Assig_1) = input("Enter marks in Assignment 1")
+int (Assig_2) = input("Enter marks in Assignment 2")
+Assig_3 = input("Enter marks in Assignment 3")
+
+Average = (Assig_1 + Assig_2 + Assig_3) / 3
+
+print("Your average grade is: ", Average)
